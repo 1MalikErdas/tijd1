5d955b4136e480543169ac5141108048793dfd44
1MalikErdas <94320329+1MalikErdas@users.noreply.github.com>
2022-01-21 00:31:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
  const time = prompt("Hoe laat is het?");
 
 if(time >= 6 && time <= 12){
-    document.body.innerHTML += `Het is ochtend. ${time}`;
+    document.body.innerHTML += `Het is ochtend ${time} uur.`;
 }
 else if(time >=12 && time < 18){
-    document.body.innerHTML += `Het is middag. ${time}`;
+    document.body.innerHTML += `Het is middag ${time} uur.`;
 }
 else if(time >= 18 && time < 24){
-    document.body.innerHTML += `Het is avond. ${time}`;
+    document.body.innerHTML += `Het is avond ${time} uur.`;
 }
 else if(time < 6 && time === 24){
-    document.body.innerHTML += `Het is nacht. ${time}`;
+    document.body.innerHTML += `Het is nacht ${time} uur.`;
 }
 else{
     document.body.innerHTML += `Vul alsjeblieft een de geldige uur in. ${time}`;
